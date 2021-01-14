04e2443ef3d19514baa3792dfc2571caf4cbc799
chcalderon <chcalderon@gmail.com>
2021-01-14 15:03:50 -0300

    execution timming

--- shell/callprocess.sh
+++ shell/callprocess.sh
@@ -6,7 +6,7 @@
 #                                                                                         #
 # Autores     : Christian Calderon / Cristian Quintana / Felipe Cruz                      #
 # Empresa     : TCS                                                                       #
-# Sintaxis    : sh callmeback.sh                                                          #
+# Sintaxis    : sh callprocess.sh                                                          #
 # Fecha       : 16/01/2021                                                                #
 #*****************************************************************************************#
 
@@ -17,7 +17,8 @@ import os;
 import sys;
 from subprocess import call;
 import logging;
-
+import time;
+start = time.time()
 log = logging.getLogger("logging_tryout2")
 
 #*****************************************************************************************#
@@ -71,7 +72,7 @@ def call_python():
 # Spark submit activos 1
 #*****************************************************************************************#
 def call_activos1():
-    log.info("En activos 1")
+    log.info("Procesando activos 1")
     cmd = "spark-submit --jars hdfs://10.128.0.3/bancochile/gdd/jar/huemul-bigdatagovernance-2.6.2.jar,hdfs://10.128.0.3/bancochile/gdd/jar/huemul-sql-decode-1.0.jar,hdfs://10.128.0.3/bancochile/gdd/jar/fdd.todos.settings-1.4.0.jar,hdfs://10.128.0.3/bancochile/gdd/jar/ojdbc7-12.1.0.1.jar,hdfs://10.128.0.3/bancochile/gdd/jar/fdd.todos.cipher-1.0.1.jar,hdfs://10.128.0.3/bancochile/gdd/jar/hive-contrib-2.3.5.jar,hdfs://10.128.0.3/bancochile/gdd/jar/hive-warehouse-connector_2.11-1.0.0.3.1.0.0-78.jar --class cl.bancochile.fdd.master.cmf.process.process_activos1_mes hdfs://10.128.0.3/bancochile/gdd/jar/fdd.master.cmf-1.0.0.jar Environment=production,RegisterInControl=false,TestPlanMode=false,year=2019,month=09,day=01,num_registros_min=1,num_registros_max=10000000,params='',krb5_env=false --deploy-mode cluster --master yarn-cluster --executor-memory 2GB --driver-memory 2GB --num-executors 2 --total-executor-cores 4 --files /opt/hive/conf/hive-site.xml"
     try:
         call(cmd, shell=True)
@@ -84,7 +85,7 @@ def call_activos1():
 
 
 def call_activos2():
-    log.info("En activos 2")
+    log.info("Procesando activos 2")
     cmd = "spark-submit --jars hdfs://10.128.0.3/bancochile/gdd/jar/huemul-bigdatagovernance-2.6.2.jar,hdfs://10.128.0.3/bancochile/gdd/jar/huemul-sql-decode-1.0.jar,hdfs://10.128.0.3/bancochile/gdd/jar/fdd.todos.settings-1.4.0.jar,hdfs://10.128.0.3/bancochile/gdd/jar/ojdbc7-12.1.0.1.jar,hdfs://10.128.0.3/bancochile/gdd/jar/fdd.todos.cipher-1.0.1.jar,hdfs://10.128.0.3/bancochile/gdd/jar/hive-contrib-2.3.5.jar,hdfs://10.128.0.3/bancochile/gdd/jar/hive-warehouse-connector_2.11-1.0.0.3.1.0.0-78.jar --class cl.bancochile.fdd.master.cmf.process.process_activos2_mes  hdfs://10.128.0.3/bancochile/gdd/jar/fdd.master.cmf-1.0.0.jar Environment=production,RegisterInControl=false,TestPlanMode=false,year=2019,month=09,day=01,num_registros_min=1,num_registros_max=10000000,params='',krb5_env=false --deploy-mode cluster --master yarn-cluster --executor-memory 2GB --driver-memory 2GB --num-executors 2 --total-executor-cores 4 --files /opt/hive/conf/hive-site.xml"
     try:
         call(cmd, shell=True)
@@ -97,7 +98,7 @@ def call_activos2():
 #*****************************************************************************************#
 
 def call_op_repo():
-    log.info("En Call Op Repo")
+    log.info("Procesando Reporte")
     cmd = "spark-submit --jars hdfs://10.128.0.3/bancochile/gdd/jar/huemul-bigdatagovernance-2.6.2.jar,hdfs://10.128.0.3/bancochile/gdd/jar/huemul-sql-decode-1.0.jar,hdfs://10.128.0.3/bancochile/gdd/jar/fdd.todos.settings-1.4.0.jar,hdfs://10.128.0.3/bancochile/gdd/jar/ojdbc7-12.1.0.1.jar,hdfs://10.128.0.3/bancochile/gdd/jar/fdd.todos.cipher-1.0.1.jar,hdfs://10.128.0.3/bancochile/gdd/jar/hive-contrib-2.3.5.jar,hdfs://10.128.0.3/bancochile/gdd/jar/hive-warehouse-connector_2.11-1.0.0.3.1.0.0-78.jar --class cl.bancochile.fdd.dim.sgt.process.process_cmf_operational_reporting  hdfs://10.128.0.3/bancochile/gdd/jar/fdd.dim.sgt-1.0.0.jar Environment=production,RegisterInControl=false,TestPlanMode=false,year=2019,month=09,day=01,num_registros_min=1,num_registros_max=10000000,params='',krb5_env=false --deploy-mode cluster --master yarn-cluster --executor-memory 2GB --driver-memory 2GB --num-executors 2 --total-executor-cores 4 --files /opt/hive/conf/hive-site.xml"
     try:
         call(cmd, shell=True)
@@ -126,7 +127,7 @@ def ayudallamada():
 #*****************************************************************************************#
 if __name__ == "__main__":
      logger_config()
-     log.info("hello logger")
+     log.info("Inicio Proceso")
      args = sys.argv[1:]
      # Si no hay dos parametros se muestra la ayuda y se termina la ejecucion
      if len(args) < 2 :
@@ -149,4 +150,7 @@ if __name__ == "__main__":
              call_activos1()
              call_activos2()
              call_op_repo()
-
+     log.info("Fin Proceso")
+     done = time.time()
+     elapsed = done - start
+     log.info("Time elapsed: "+time.strftime('%H:%M:%S', time.gmtime(elapsed)))
